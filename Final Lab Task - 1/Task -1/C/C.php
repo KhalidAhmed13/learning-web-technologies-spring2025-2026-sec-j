98e5ffbd058fc3b3f10d2094f3ccb7809781e457
KhalidAhmed13 <rishadsheik1122@gmail.com>
2026-04-12 15:01:01 +0600

    Final task-1(C)

---
+++ Final Lab Task - 1/Task -1/C/C.php
@@ -0,0 +1,31 @@
+<?php
+    $username = "";
+    $message = "";
+
+    if(isset($_REQUEST['submit'])){
+
+        $username = $_REQUEST['username'];
+        
+        if($username == ""){
+            $message = "null name!";
+        }else{
+            $message = "Entered Name: ".$username;
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Name Input</title>
+</head>
+<body> 
+    <form method="post" action="C.php" enctype="multipart/form-data">
+        Name: <input type="text" name="username" value="<?php if(isset($_POST['username'])){echo $username;} ?>"/> <br>
+        <input type="submit" name="submit" value="Submit"/>
+    </form>
+
+    <br>
+    <?php echo $message; ?>
+</body>
+</html>
