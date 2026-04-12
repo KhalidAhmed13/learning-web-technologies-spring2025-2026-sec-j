69c087be54edf8e64ddc675e175f06dd19aae8ec
KhalidAhmed13 <rishadsheik1122@gmail.com>
2026-04-12 15:44:54 +0600

    Final task-3(B)

---
+++ Final Lab Task - 1/Task - 3/B/B.php
@@ -0,0 +1,41 @@
+<?php
+    $message = "";
+
+    if(isset($_REQUEST['submit'])){
+
+        $dd = $_REQUEST['dd'];
+        $mm = $_REQUEST['mm'];
+        $yyyy = $_REQUEST['yyyy'];
+
+        if($dd == "" || $mm == "" || $yyyy == ""){
+            $message = "null date of birth!";
+        }else{
+            $message = "Entered Date of Birth: ".$dd."/".$mm."/".$yyyy;
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Date of Birth</title>
+</head>
+<body>
+    <form method="post" action="B.php" enctype="multipart/form-data">
+        <fieldset style="width: 500px;">
+            <legend><b>DATE OF BIRTH</b></legend>
+
+            dd &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; mm &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; yyyy <br>
+            <input type="text" name="dd" size="5"/> /
+            <input type="text" name="mm" size="5"/> /
+            <input type="text" name="yyyy" size="8"/>
+            <br><br><hr><br>
+
+            <input type="submit" name="submit" value="Submit"/>
+        </fieldset>
+    </form>
+
+    <br>
+    <?php echo $message; ?>
+</body>
+</html>
